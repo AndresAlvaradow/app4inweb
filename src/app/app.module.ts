540989ea20f857f8623359e8b195e39a25150689
AndresAlvaradow <andresalvarado34@gmail.com>
2020-07-10 18:03:34 -0500

    segunda subida a heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MiComponente } from './componentes/mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
   ],
   providers: [],
   bootstrap: [AppComponent]
